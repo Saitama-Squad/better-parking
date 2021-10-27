9803bba32af88931f038188555048c1e54ef8040
lokeshn011101 <lokesh19055@cse.ssn.edu.in>
2021-10-27 14:45:39 +0530

    added producer, consumer for kafka

--- VMs/EventStream.py
+++ VMs/EventStream.py
@@ -1,12 +1,27 @@
 import wiotp.sdk.application
 from wiotp.sdk.api.services import EventStreamsServiceBindingCredentials, EventStreamsServiceBindingCreateRequest
 import json
+import os
+from kafka import KafkaProducer
+from kafka.errors import KafkaError
+from dotenv import load_dotenv
+load_dotenv()
+
+bootstrap_kafka_servers = 'broker-4-22l15k93cvmwf56x.kafka.svc07.us-south.eventstreams.cloud.ibm.com:9093,broker-3-22l15k93cvmwf56x.kafka.svc07.us-south.eventstreams.cloud.ibm.com:9093,broker-1-22l15k93cvmwf56x.kafka.svc07.us-south.eventstreams.cloud.ibm.com:9093,broker-0-22l15k93cvmwf56x.kafka.svc07.us-south.eventstreams.cloud.ibm.com:9093,broker-2-22l15k93cvmwf56x.kafka.svc07.us-south.eventstreams.cloud.ibm.com:9093,broker-5-22l15k93cvmwf56x.kafka.svc07.us-south.eventstreams.cloud.ibm.com:9093'.split(
+    ',')
+print("Starting Connection")
+producer = KafkaProducer(bootstrap_servers=bootstrap_kafka_servers,
+                         security_protocol='SASL_SSL', sasl_mechanism='PLAIN', sasl_plain_username='username', sasl_plain_password=os.environ.get('SASL_PLAIN_PASSWORD'), value_serializer=lambda m: json.dumps(m).encode('ascii'))
+print("Kafka Broker Connected!")
 
 
 def eventCallback(event):
-    str = "%s event '%s' received from device [%s]: %s"
-    print(str % (event.format, event.eventId,
-          event.device, json.dumps(event.data)))
+    # str = "%s event '%s' received from device [%s]: %s"
+    # print(str % (event.format, event.eventId,
+    #       event.device, json.dumps(event.data)))
+    data = json.dumps(event.data)
+    producer.send('alldata',  {'data': data})
+    print(f"Event {event.eventId} sent to kafka server!")
 
 
 config = {
