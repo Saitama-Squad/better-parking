baab9b6766e78f0433729a155caf33f783d21365
monieshravichandrran <monieshravichandrran@gmail.com>
2021-11-16 13:56:31 +0530

    Adding Login Page

--- src/Login.js
+++ src/Login.js
@@ -1,75 +1,106 @@
-import React from "react";
+import React,{useState} from "react";
 import Avatar from "./images/avatar.png";
-
+import Font from "react-font";
+import Loader from "./Loader";
+import { collection, query, where, getDocs } from "firebase/firestore";
+import { getAuth, signInWithEmailAndPassword } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+import firebase from "./firebaseConfig";
 const Login = ({ setPage }) => {
-  const loginHandler = (e) => {
+  const db = getFirestore()
+  const [loader, setLoader] = useState(false);
+  const [error, setError] = useState("");
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const loginHandler = async(e) => {
     e.preventDefault();
-    setPage("floors");
+    setLoader(true);
+    setError("")
+    console.log(process.env.REACT_APP_API_KEY)
+    if (username.length === 0) {
+      setError("Enter Username")
+    }
+    else if (password.length === 0) {
+      setError("Enter Password")
+    }
+    else {
+      const auth = getAuth();
+      try {
+        const userCredential = await signInWithEmailAndPassword(
+          auth,
+          username,
+          password
+        );
+        const user = userCredential.user;
+        console.log(user)
+      }
+      catch {
+
+      }
+    }
+    setLoader(false);
+    //setPage("floors");
   };
 
   return (
-    <div className="px-16 mt-10">
-      <h1 className="text-6xl text-center pb-7">
-        Better Parking Welcomes You!!!
-      </h1>
-      <form className="border-2 border-black border-solid flex flex-col justify-around items-center">
-        <div className="mt-6 mb-3 flex items-center justify-center">
-          <img src={Avatar} alt="Avatar" className="w-4/12 rounded-full" />
+    <div className="relative">
+      <Loader show={loader} />
+      <div className="h-screen w-screen" style={{
+        background:
+          "linear-gradient(rgba(0,0,0,0.7),rgba(0,0,0,0.7))",
+      }}>
+        <div className="px-16 pt-12">
+          <Font family="Monoton"><h1 className="text-6xl text-center text-white">Better-Parking Welcomes you</h1></Font>
         </div>
+        <form className="flex flex-col justify-around items-center">
+          <div className="mt-6 mb-3 flex items-center justify-center">
+            <img src={Avatar} alt="Avatar" className="w-4/12 rounded-full" />
+          </div>
 
-        <div className="p-4 w-full">
-          <label htmlFor="uname">
-            <b>Username</b>
-          </label>
-          <input
-            className="w-full py-3 px-5 my-2 border-2 border-solid border-gray-200 flex justify-center items-center"
-            type="text"
-            placeholder="Enter Username"
-            name="uname"
-            method="post"
-            required
-          />
+          <div className="p-4 w-1/2">
+            <label htmlFor="uname" className="text-white">
+              <b>Username</b>
+            </label>
+            <input
+              className="w-full py-3 px-5 my-2 border-2 border-solid border-gray-200 flex justify-center items-center"
+              type="text"
+              placeholder="Enter Username"
+              name="uname"
+              method="post"
+              onChange={(event) => {
+                setUsername(event.target.value)
+              }}
+              required
+            />
 
-          <label htmlFor="psw">
-            <b>Password</b>
-          </label>
-          <input
-            className="w-full py-3 px-5 my-2 border-2 border-solid border-gray-200 flex justify-center items-center"
-            type="password"
-            placeholder="Enter Password"
-            name="psw"
-            method="post"
-            required
-          />
-
-          <button
-            type="submit"
-            className="bg-green-600 text-white py-3 px-5 my-2 mx-0 border-none cursor-pointer w-full hover:opacity-80"
-            onClick={loginHandler}
-          >
-            Login
-          </button>
-          <label>
-            <input type="checkbox" checked="checked" name="remember" /> Remember
-            me
-          </label>
-        </div>
-
-        <div
-          className="p-4 flex flex-row w-full items-center justify-around"
-          style={{ backgroundColor: "#f1f1f1" }}
-        >
-          <button
-            type="button"
-            className="w-auto py-3 px-4 bg-red-500 text-white"
-          >
-            Register
-          </button>
-          <span>
-            <a href="/">Forgot password?</a>
-          </span>
-        </div>
-      </form>
+            <label htmlFor="psw" className="text-white">
+              <b>Password</b>
+            </label>
+            <input
+              className="w-full py-3 px-5 my-2 border-2 border-solid border-gray-200 flex justify-center items-center"
+              type="password"
+              placeholder="Enter Password"
+              name="psw"
+              method="post"
+              onChange={(event) => {
+                setPassword(event.target.value)
+              }}
+              required
+            />
+            <div className="flex justify-center text-red-500 text-lg">
+              {error}
+            </div>  
+            <button
+              type="submit"
+              className="bg-green-600 text-white py-3 px-5 my-2 mx-0 border-none cursor-pointer w-full hover:opacity-80"
+              onClick={loginHandler}
+            >
+              Login
+            </button>
+          </div>
+        </form>
+      </div>
+      
     </div>
   );
 };
